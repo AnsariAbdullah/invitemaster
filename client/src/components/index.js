a805dbffa536a8175d5237a1a0f41d54834ce9dc
AnsariAbdullah <ansariabdullahar@gmail.com>
2026-03-04 07:29:46 +0530

    add components

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,1 +1,5 @@
 export { default as NavBar } from './NavBar';
+export { default as FormRow } from './FormRow';
+export { default as Logo } from './Logo';
+export { default as SmallSidebar } from './SmallSidebar';
+export { default as BigSidebar } from './BigSidebar';
